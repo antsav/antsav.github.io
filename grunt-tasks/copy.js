596a3f8c82b1a5b0c0e0423c1d1c10d295d74e63
Anton <antsav@gmail.com>
2016-11-24 19:22:19 -0800

    fonts to be copied from src to build folder

--- grunt-tasks/copy.js
+++ grunt-tasks/copy.js
@@ -1,17 +1,16 @@
 module.exports = function copy(grunt) {
     // Load task
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-copy')
 
     // Options
     return {
         main: {
-          files: [
+            files: [
             {expand: true, cwd: 'src/img/', src: ['**'], dest: 'build/img', filter: 'isFile'},
             {expand: true, cwd: 'src/fonts/', src: ['**'], dest: 'build/fonts', filter: 'isFile'},
 
             {'./index.html': './src/index.html'},
-          ],
+            ],
         },
     };
-
 };
